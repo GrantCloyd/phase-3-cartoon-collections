28db4009cc10538c7c72ded62c9e7d444459ecd3
Grant Cloyd <dgcloyd@gmail.com>
2021-06-27 10:00:26 -0500

    commit tree f7463e0effe9bd2da43e35a40ec56dc0b8e3ce4e

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,22 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+require 'pry'
+
+def roll_call_dwarves arr
+  arr.each_with_index {|dwarf, index| puts "#{index +1}. #{dwarf}"}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet arr
+  arr.map {|element| element.capitalize << "!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls arr
+  arr.any? {|call| call.length > 4}
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese arr
   cheese_types = ["cheddar", "gouda", "camembert"]
+  arr.detect {|entry| entry.include? {|i| (cheese_types.length).times cheese_types[i] } 
+
+
 end
+
+ 
